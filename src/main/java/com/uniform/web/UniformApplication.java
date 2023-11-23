c993438a4f21d66572c22fdd8ebae375060c68b4
hojun <rhzn5512@naver.com>
2023-11-23 19:55:36 +0900

    [Set] 초기 환경 설정 추가
    롬복, 어노테이션, 타임리프, JPA, mySql 드라이버 추가

--- src/main/java/com/uniform/web/UniformApplication.java
+++ src/main/java/com/uniform/web/UniformApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UniformApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UniformApplication.class, args);
 	}
-
 }
