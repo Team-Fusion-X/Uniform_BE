6e03928daa12d459ee0d7fc9b64d67a1fd6b745a
hojoon123 <rhzn5512@naver.com>
2023-12-07 16:17:55 +0900

    [Fix] 코드 리팩토링 MVC -> RestAPI 화 진행
    회원가입 및 내정보조회 API화

--- src/main/java/com/uniform/web/member/controller/MemberController.java
+++ src/main/java/com/uniform/web/member/controller/MemberController.java
@@ -3,61 +3,59 @@ package com.uniform.web.member.controller;
 import com.uniform.web.member.dto.MemberDTO;
 import com.uniform.web.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
 
-@Controller
-@RequiredArgsConstructor //MemberService에 대한 멤버를 사용 가능
+@RestController
+@RequestMapping("/api/members") // 엔드포인트를 명시적으로 지정
+@RequiredArgsConstructor
 public class MemberController {
 
     // 생성자 주입
     private final MemberService memberService;
 
     // 회원가입 페이지 출력 요청
-    @GetMapping("/member/save")
+    @GetMapping("/save")
     public String saveForm() {
         return "save";
     }
-    @GetMapping("/member/check")
+    @GetMapping("/check")
     public String checkForm(){ return "check"; }
-    @PostMapping("/member/check")
-    @ResponseBody
-    public MemberDTO checkMember(@ModelAttribute MemberDTO memberDTO, HttpServletResponse response){
+    @PostMapping("/check")
+    public ResponseEntity<?> checkMember(@RequestBody MemberDTO memberDTO, HttpServletResponse response) {
         String memberId = memberDTO.getMemberId();
         MemberDTO userInfo = memberService.FindByMemberId(memberId);
+
         if (userInfo == null) {
-            response.setStatus((HttpServletResponse.SC_BAD_REQUEST));
-            return null;
-        }
-        else{
-            return userInfo;
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } else {
+            return ResponseEntity.ok(userInfo);
         }
     }
-    @PostMapping("/member/save")    // name값을 requestparam에 담아온다
-    public String save(@ModelAttribute MemberDTO memberDTO, HttpServletResponse response) {
-        System.out.println("MemberController.save");
-        System.out.println("memberDTO = " + memberDTO);
-
-        // 비밀번호와 비밀번호 확인이 일치하지 않으면 에러 메시지를 모델에 추가하고 회원가입 페이지로 이동
+    @PostMapping("/save")
+    public ResponseEntity<?> save(@RequestBody MemberDTO memberDTO, HttpServletResponse response) {
         if (!memberDTO.isPasswordMatch()) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return null;  // 뷰 이름이 필요x
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("{\"data\": \"비밀번호가 일치하지 않습니다.\"}");
         }
-        //현재 memberDTO의 아이디가 데이터베이스에 있는지 확인하는 메소드 MemberService 클래스에 정의
-        if (memberService.isExistId(memberDTO)){
-            response.setStatus((HttpServletResponse.SC_BAD_REQUEST));
-            System.out.println("중복");
-            return null; //
+
+        if (memberService.isExistId(memberDTO)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("{\"data\": \"이미 존재하는 아이디입니다.\"}");
         }
-        // 성공
-        memberService.save(memberDTO);
-        // 여기 200 리퀘스트로 변경하기만 하면 됨
-        return "hello";
+
+        try {
+            memberService.save(memberDTO);
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } catch (Exception e) {
+            // 저장 실패 시에도 실패 응답을 반환합니다.
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("{\"data\": \"회원가입에 실패하였습니다.\"}");
+        }
     }
 }
