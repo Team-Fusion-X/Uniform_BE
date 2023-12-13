a338b5effae239966eed8729f049dd267db5c985
hyohyo12 <ehdgy9999@naver.com>
2023-12-13 22:36:28 +0900

    [Feat]입시 정보 조회 역직렬화 기능
    입시정보 json 객체로 역직렬화 기능 구현

---
+++ src/main/java/com/uniform/web/AdmsissionInform/Repository/AdmissionInformRepository.java
@@ -0,0 +1,8 @@
+//package com.uniform.web.AdmsissionInform.Repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface AdmissionInformRepository extends JpaRepository<AdmissionInformRepository,Long> {
+//    //추가적인 메소드 구현
+//
+//}
