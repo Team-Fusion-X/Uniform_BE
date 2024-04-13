454022f1bc02164ff22b7fa295764317f5da15ec
hyohyo12 <ehdgy9999@naver.com>
2024-04-13 16:06:14 +0900

    [Feat] 과목 삽입 작업시작
    작업 시작했습니다

--- src/main/java/com/uniform/web/AdmsissionInform/entity/scoreEntity.java
+++ src/main/java/com/uniform/web/AdmsissionInform/entity/scoreEntity.java
@@ -1,7 +1,9 @@
 package com.uniform.web.AdmsissionInform.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.uniform.web.member.entity.MemberEntity;
+import com.uniform.web.member.repository.MemberRepository;
+import jakarta.persistence.*;
+import jakarta.servlet.http.HttpSession;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,5 +14,22 @@ import lombok.Setter;
 @NoArgsConstructor
 @Table(name = "score")
 public class scoreEntity {
+    @Id
+    @Column(name = "score_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int score_id;
+
+    @OneToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "MemberEntity")
+    @Column(name = "user_id")
+    private String user_id = MemberEntity.
+
+
+
+    @Column(name ="school_year_id")
+    private int school_id;
+
+    @Column(name ="subject_id")
+    private String subject_id;
 
 }
--- src/main/java/com/uniform/web/member/repository/MemberRepository.java
+++ src/main/java/com/uniform/web/member/repository/MemberRepository.java
@@ -1,7 +1,6 @@
 package com.uniform.web.member.repository;
 
 import com.uniform.web.member.entity.MemberEntity;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
